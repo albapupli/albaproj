c7c413d99060ae4cdf361e6fac780793f4effd65
Alba Pupli <apupli19@epoka.edu.al>
2024-06-10 00:09:13 +0200

    docker compose for mysql and app

---
+++ src/main/resources/db/migration/script.sql
@@ -0,0 +1,13 @@
+-- Create users table
+CREATE TABLE users (
+   id BIGINT AUTO_INCREMENT PRIMARY KEY,
+   first_name VARCHAR(255) NOT NULL,
+   middle_name VARCHAR(255),
+   last_name VARCHAR(255) NOT NULL,
+   phone_number VARCHAR(20),
+   date_of_birth VARCHAR(10),
+   email VARCHAR(255) NOT NULL,
+   password VARCHAR(255) NOT NULL,
+   profile_photo_path VARCHAR(255),
+   is_admin BOOLEAN DEFAULT FALSE
+);
